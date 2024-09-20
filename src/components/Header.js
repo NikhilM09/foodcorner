483d0edc944a739a9735943f7904723fd6067433
NikhilM09 <mahadiknikhil28@gmail.com>
2024-09-20 20:52:29 +0530

    text-color changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export const Header = () =>{
     return(
-        <div className="text-purple-950 text-7xl font-extrabold">This is Header Component</div>
+        <div className="text-purple-800 text-7xl font-extrabold">This is Header Component</div>
     )
 }
 
